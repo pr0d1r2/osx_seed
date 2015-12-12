0c4288634ab9494b4d94eb617bc13ee65367ae31
Marcin Nowicki <pr0d1r2@gmail.com>
2015-12-12 10:32:53 +0100

    Keep kitchenplan config up-to-date

--- setup.sh
+++ setup.sh
@@ -4,7 +4,10 @@ if [ ! -f ~/.gem/ruby/2.0.0/bin/kitchenplan ]; then
   gem install --no-ri --no-rdoc --user-install kitchenplan || return $?
 fi
 
-if [ ! -d /opt/kitchenplan ]; then
+if [ -d /opt/kitchenplan ]; then
+  cd /opt/kitchenplan || return $?
+  git pull || return $?
+else
   ~/.gem/ruby/2.0.0/bin/kitchenplan setup \
     --config \
     --gitrepo=git@github.com:pr0d1r2/kitchenplan-config.git \
